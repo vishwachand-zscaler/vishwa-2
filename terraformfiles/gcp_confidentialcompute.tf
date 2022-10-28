c9f33bb59a502e4b38d35cb56604e231e10f17ba
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-10-28 13:03:12 +0530

    Update gcp_confidentialcompute.tf

--- terraformfiles/gcp_confidentialcompute.tf
+++ terraformfiles/gcp_confidentialcompute.tf
@@ -2,7 +2,7 @@ provider "google" {
   project     = "my-project-id"
   region      = "us-central1"
 }
-resource "google_compute_instance" "confidential" {
+resource "google_compute_instance" "confidential1" {
   name         = "test"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
